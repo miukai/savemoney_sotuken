ce1c985162335abb3cd55632d927d8ae1cfed333
magruru <rubii.space@icloud.com>
2020-11-20 09:26:33 +0900

    marge

--- app/src/main/java/com/example/savemoney/MapsActivity.kt
+++ app/src/main/java/com/example/savemoney/MapsActivity.kt
@@ -1,7 +1,11 @@
 package com.example.savemoney
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.app.ActivityCompat
 import androidx.lifecycle.Transformations.map
 
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -9,7 +13,12 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.GoogleMap.InfoWindowAdapter
+
+
+
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -37,12 +46,69 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap.uiSettings.isZoomControlsEnabled = true
 
         mMap.setOnMapClickListener(object :GoogleMap.OnMapClickListener {
-
+        // Map上の任意の場所にマーカーを立てる
             override fun onMapClick(latlng :LatLng) {
                 val location = LatLng(latlng.latitude,latlng.longitude)
                 mMap.addMarker(MarkerOptions().position(location))
             }
         })
+
+        googleMap.uiSettings.isCompassEnabled = true
+        // コンパスを表示
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        // 位置情報を取得するための許可
+        googleMap.isMyLocationEnabled = true
+        // 位置情報取得
+
+        googleMap.isMyLocationButtonEnabled = true
+        // 現在地ボタン
+
+
+
+//        class CustomInfoWindowAdapter : InfoWindowAdapter {
+//
+//            // These are both view groups containing an ImageView with id "badge" and two
+//            // TextViews with id "title" and "snippet".
+//            private val window: View = layoutInflater.inflate(R.layout.custom_info_window, null)
+//            private val contents: View = layoutInflater.inflate(R.layout.custom_info_contents, null)
+//
+//            override fun getInfoWindow(marker: Marker): View? {
+//                if (options.checkedRadioButtonId != R.id.custom_info_window) {
+//                    // This means that getInfoContents will be called.
+//                    return null
+//                }
+//                render(marker, window)
+//                return window
+//            }
+//
+//            override fun getInfoContents(p0: Marker?): View {
+//                TODO("Not yet implemented")
+//            }
+//        }
+
+
+
+//            mMap.setInfoWindowAdapter(object :GoogleMap.InfoWindowAdapter{
+//                override fun getInfoWindow(marker : Marker): View? {
+//                    return null;
+//                }
+//
+//                override fun getInfoContents(p0: Marker?): View {
+//                    TODO("Not yet implemented")
+//                }
+//            })
+
+
 
         // Add a marker in Sydney and move the camera
         val sydney = LatLng(-34.0, 151.0)
