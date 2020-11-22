f0cfd83c2cb4f7c16632966f5d3206542da23fe7
miukai <60338199+miukai@users.noreply.github.com>
2020-11-22 11:24:44 +0900

    MemoScreen transition creation

---
+++ app/src/main/java/com/example/savemoney/CreateMemo.kt
@@ -0,0 +1,14 @@
+package com.example.savemoney
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class CreateMemo : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        return  inflater.inflate(R.layout.create_memo, container, false)
+    }
+}
--- app/src/main/java/com/example/savemoney/MainActivity.kt
+++ app/src/main/java/com/example/savemoney/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.savemoney
 
-import androidx.appcompat.app.AppCompatActivity
+//import gitandroidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI.setupWithNavController
