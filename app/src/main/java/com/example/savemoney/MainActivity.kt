9bebb62024f2bea3ad0791f839b0f9ae9dfbf8c9
miukai <60338199+miukai@users.noreply.github.com>
2020-12-03 13:45:09 +0900

    create date

--- app/src/main/java/com/example/savemoney/MainActivity.kt
+++ app/src/main/java/com/example/savemoney/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.savemoney
 
 //import gitandroidx.appcompat.app.AppCompatActivity
+import android.app.DatePickerDialog
+import android.app.Dialog
 import android.content.Intent
 import android.os.Bundle
+import android.text.format.DateFormat
 import android.view.View
 import android.widget.Button
+import android.widget.DatePicker
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.add
 import androidx.navigation.Navigation
@@ -23,22 +29,31 @@ import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import java.util.*
 
-class MainActivity : AppCompatActivity(),EnterForm.OnButtonClickListener {
+class MainActivity : AppCompatActivity(),EnterForm.OnButtonClickListener,MapFragment.OnShowCurrentDate,MapFragment.OnMap{
     private var currentDate = Calendar.getInstance()
-    private lateinit var googleMap: GoogleMap
+    private lateinit var googleMap:GoogleMap
+    override fun onMap() {
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.navi_map) as? SupportMapFragment
+        mapFragment?.getMapAsync {
+            googleMap = it
+            renderMap()
+        }
+    }
+    override fun onShowCurrentDate() {
+            val dialog = DatePickerFragment()
+            dialog.arguments = Bundle().apply{
+                putSerializable("current","currentDate")}
+            dialog.show(supportFragmentManager,"calendar")
+    }
     override fun onButtonClicked(){
         if (supportFragmentManager.findFragmentByTag("productFragment1")== null){
             supportFragmentManager.beginTransaction()
                     .add(R.id.container,newProductFragment(),"productFragment1")
-                    .commit()
-            supportFragmentManager.beginTransaction()
                     .add(R.id.container, newPriceFragment(),"priceFragment1")
                     .commit()
         }else if (supportFragmentManager.findFragmentByTag("productFragment2")== null){
             supportFragmentManager.beginTransaction()
                     .add(R.id.container2,newProductFragment(),"productFragment2")
-                    .commit()
-            supportFragmentManager.beginTransaction()
                     .add(R.id.container2,newPriceFragment(),"priceFragment2")
                     .commit()
         }
@@ -46,14 +61,17 @@ class MainActivity : AppCompatActivity(),EnterForm.OnButtonClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val mapFragment = supportFragmentManager.findFragmentById(R.id.navi_map) as SupportMapFragment?
-        mapFragment?.getMapAsync{
-            googleMap = it
-            renderMap()
-        }
         val navView:BottomNavigationView = findViewById(R.id.bottom_navigation)
         val navController = findNavController(R.id.nav_host_fragment)
         navView.setupWithNavController(navController)
+    }
+    private fun renderMap(){
+        val locations = selectInDay(this,
+                currentDate[Calendar.YEAR],currentDate[Calendar.MONTH],
+                currentDate[Calendar.DATE])
+
+        zoomTo(googleMap,locations)
+        putMarkers(googleMap,locations)
     }
 
 //    override fun onMapReady(googleMap: GoogleMap) {
@@ -63,14 +81,6 @@ class MainActivity : AppCompatActivity(),EnterForm.OnButtonClickListener {
 //        map.addMarker(MarkerOptions().position(tokyo).title("東京"))
 //        map.moveCamera(CameraUpdateFactory.newLatLngZoom(tokyo,15f))
 //    }
-    private fun renderMap(){
-        val locations = selectInDay(this,
-        currentDate[Calendar.YEAR],currentDate[Calendar.MONTH],
-        currentDate[Calendar.DATE])
-
-        zoomTo(googleMap,locations)
-        putMarkers(googleMap,locations)
-    }
     private fun zoomTo(map: GoogleMap,locations:List<LocationRecord>){
         if (locations.size == 1){
             val latLng = LatLng(locations[0].latitude,locations[0].longitude)
@@ -93,7 +103,41 @@ class MainActivity : AppCompatActivity(),EnterForm.OnButtonClickListener {
             map.moveCamera(move)
         }
     }
-    private fun putMarkers(map:GoogleMap,locations: List<LocationRecord>){
+    private fun putMarkers(map:GoogleMap,locations: List<LocationRecord>) {
 
     }
 }
+//    private fun showCurrentDate(){
+//        val dateView = supportFragmentManager.findFragmentById(R.id.date) as TextView?
+//        dateView?.setOnClickListener{
+//            val dialog = DatePickerFragment()
+//            dialog.arguments = Bundle().apply{
+//                putSerializable("current","currentDate")}
+//            dialog.show(supportFragmentManager,"calendar")
+//            }
+//        dateView?.setText(DateFormat.format("MM月 dd日",currentDate.time))
+//        }
+
+
+
+//class DatePickerFragment : DialogFragment(),DatePickerDialog.OnDateSetListener{
+//    private lateinit var calendar : Calendar
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        calendar = arguments?.getSerializable("current") as? Calendar
+//                ?:Calendar.getInstance()
+//    }
+//
+//    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+//        return DatePickerDialog(requireContext(),this,
+//        calendar[Calendar.YEAR], calendar[Calendar.MONTH],
+//        calendar[Calendar.DATE])
+//    }
+//    override fun onDateSet(view:DatePicker,year:Int,month:Int,day:Int){
+//        if (context is DatePickerDialog.OnDateSetListener){
+//            (context as DatePickerDialog.OnDateSetListener).onDateSet(
+//                    view,year,month,day
+//            )
+//        }
+//    }
+//}
--- app/src/main/java/com/example/savemoney/MapFragment.kt
+++ app/src/main/java/com/example/savemoney/MapFragment.kt
@@ -1,13 +1,19 @@
 package com.example.savemoney
 
 
+import android.app.DatePickerDialog
+import android.app.Dialog
 import android.content.Context
 import android.os.Bundle
+import android.text.format.DateFormat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.DatePicker
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.findFragment
 import androidx.navigation.findNavController
@@ -19,30 +25,112 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.LatLngBounds
 import com.google.android.gms.maps.model.MarkerOptions
+import java.lang.RuntimeException
 import java.util.*
 
-class MapFragment : Fragment(){
-    private lateinit var googleMap: GoogleMap
+class MapFragment : Fragment(),DatePickerDialog.OnDateSetListener{
+    private var currentDate = Calendar.getInstance()
+    private lateinit var googleMap:GoogleMap
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         super.onCreateView(inflater, container, savedInstanceState)
-        return inflater.inflate(R.layout.activity_map, container, false)
+        val view = inflater.inflate(R.layout.activity_map, container, false)
+        val listenerMap = context as? OnMap
+        listenerMap?.onMap()
+        return view
         }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val dateView = view?.findViewById(R.id.date) as TextView?
+        dateView?.setOnClickListener {
+            val listener = context as? OnShowCurrentDate
+            listener?.onShowCurrentDate()
+            dateView?.setText(DateFormat.format("MM月 dd日",currentDate.time))
+        }
+
         // Fragment を使った Nav Controller の取得
         val navController = this.findNavController()
         val button = view.findViewById<Button>(R.id.Memobutton)
         button.setOnClickListener {
             navController.navigate(R.id.action_navi_map_to_navi_create_memo)
         }
-
-//         View を使った Nav Controller の取得
-//        val button = view.findViewById<Button>(R.id.nav_button)
-//        button.setOnClickListener { view ->
-//            val navController = view.findNavController()
-//            navController.navigate(R.id.action_oneFragment_to_twoFragment)
-//        }
+    }
+    override fun onDateSet(view:DatePicker?,
+                           year: Int,month:Int,dayOfMonth:Int){
+        //選択中の日付を更新
+        currentDate.set(year,month,dayOfMonth)
+        renderMap()
+        val listener = context as? OnShowCurrentDate
+        listener?.onShowCurrentDate()//日付のテキストビューを更新
     }
 
+    private fun renderMap(){
+        val locations = selectInDay(requireContext(),
+                currentDate[Calendar.YEAR],currentDate[Calendar.MONTH],
+                currentDate[Calendar.DATE])
 
+        zoomTo(googleMap,locations)
+        putMarkers(googleMap,locations)
+    }
+    private fun zoomTo(map: GoogleMap,locations:List<LocationRecord>){
+        if (locations.size == 1){
+            val latLng = LatLng(locations[0].latitude,locations[0].longitude)
+
+            val move = CameraUpdateFactory.newLatLngZoom(latLng,15f)
+            map.moveCamera(move)
+        }else if (locations.size > 1){
+            val bounds = LatLngBounds.Builder()
+            locations.forEach{location->
+                bounds.include(LatLng(location.latitude, location.longitude))
+            }
+            val padding = (50 * resources.displayMetrics.density).toInt()
+
+            val move = CameraUpdateFactory.newLatLngBounds(bounds.build(),
+                    resources.displayMetrics.widthPixels,
+                    resources.displayMetrics.heightPixels,
+                    padding)
+
+
+            map.moveCamera(move)
+        }
+    }
+    private fun putMarkers(map:GoogleMap,locations: List<LocationRecord>) {
+
+    }
+    interface OnMap{
+        fun onMap()
+    }
+    interface OnShowCurrentDate{
+        fun onShowCurrentDate()
+    }
+    interface OnTextViewClickListener{
+        fun onTextViewClicked()
+    }
 }
+class DatePickerFragment : DialogFragment(),DatePickerDialog.OnDateSetListener{
+    private lateinit var calendar : Calendar
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        calendar = arguments?.getSerializable("current") as? Calendar
+                ?:Calendar.getInstance()
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return DatePickerDialog(requireContext(),this,
+                calendar[Calendar.YEAR], calendar[Calendar.MONTH],
+                calendar[Calendar.DATE])
+    }
+    override fun onDateSet(view:DatePicker,year:Int,month:Int,day:Int){
+        if (context is DatePickerDialog.OnDateSetListener){
+            (context as DatePickerDialog.OnDateSetListener).onDateSet(
+                    view,year,month,day
+            )
+        }
+    }
+}
+
+//        val dateView = view.findViewById<TextView>(R.id.date)
+//                dateView.setOnClickListener {
+//                    val listener = context as? OnTextViewClickListener
+//                    listener?.onTextViewClicked()
+//                }
+//        dateView.setText(DateFormat.format("MM月 dd日",currentDate.time))
