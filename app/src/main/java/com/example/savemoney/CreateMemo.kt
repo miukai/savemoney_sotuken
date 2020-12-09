01f966037cd59eb2e34dd3780e820ffb80963fe7
miukai <60338199+miukai@users.noreply.github.com>
2020-12-09 11:18:59 +0900

    create Marker

--- app/src/main/java/com/example/savemoney/CreateMemo.kt
+++ app/src/main/java/com/example/savemoney/CreateMemo.kt
@@ -1,6 +1,5 @@
 package com.example.savemoney
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -11,7 +10,8 @@ import androidx.fragment.app.Fragment
 import android.os.Build
 import androidx.annotation.RequiresApi
 import java.time.LocalDate
-import android.widget.LinearLayout
+import androidx.navigation.fragment.findNavController
+
 //import androidx.navigation.fragment.findNavController
 
 class  CreateMemo : Fragment() {
@@ -27,9 +27,7 @@ class  CreateMemo : Fragment() {
 
     val ido = 0
     val kedo = 0
-    val hantei :String = "見振り分け"
-
-
+    val hantei :String = "未振り分け"
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -38,32 +36,39 @@ class  CreateMemo : Fragment() {
         var nomContext = requireContext().applicationContext
 
 //        setContentView(R.layout.create_memo)
-        val button = view.findViewById<Button>(R.id.button6)
+        val navController = this.findNavController()
+        val button = view.findViewById<Button>(R.id.conf_button)
         button.setOnClickListener {
-            val  editText = view.findViewById<EditText>(R.id.productNameID)
-            val  editText2 = view.findViewById<EditText>(R.id.priceID)
-            insertText(nomContext,editText.text.toString(),
-                    Integer.parseInt(editText2.text.toString()),
+            val product = view.findViewById(R.id.productName) as EditText
+            val productId = product.text.toString()
+            val price = view.findViewById<EditText>(R.id.price)
+            val priceId = Integer.parseInt(price.text.toString())
+            insertText(nomContext,productId,
+                    priceId,
                     nowDateString,
                     ido,
                     kedo,
                     hantei
 
             )
+            navController.navigate(R.id.action_navi_create_memo_to_navi_map)
         }
+        //動的にViewを追加する
+//        val product = view.findViewById<EditText>(R.id.productNameID)
+//        val price = view.findViewById<EditText>(R.id.priceID)
+        val addButton = view.findViewById<Button>(R.id.addForm)
+        var c = 1
+        addButton.setOnClickListener {
+            if (c < 4){                val listnear = context as? OnAddForm
 
+                listnear?.onAddForm(c)
+                c += 1
+            }else {
+                addButton.error ="これ以上入力追加出来ません"
+            }
+        }
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
+    interface OnAddForm{
+        fun onAddForm(c:Int)
+    }
 }
--- app/src/main/java/com/example/savemoney/PriceFragment.kt
+++ app/src/main/java/com/example/savemoney/PriceFragment.kt
@@ -4,12 +4,24 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 
 class PriceFragment : Fragment(){
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         super.onCreateView(inflater, container, savedInstanceState)
-        return  inflater.inflate(R.layout.fragment_price, container, false)
+        return inflater.inflate(R.layout.fragment_price, container, false)
+    }
+
+//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        super.onViewCreated(view, savedInstanceState)
+//        val price = view.findViewById<EditText>(R.id.price)
+//        val priceId = Integer.parseInt(price.text.toString())
+//        val listner = context as? OnPrice
+//        listner?.onPrice(priceId)
+//    }
+    interface OnPrice{
+        fun onPrice(price:Int)
     }
 }
 fun newPriceFragment():PriceFragment{
